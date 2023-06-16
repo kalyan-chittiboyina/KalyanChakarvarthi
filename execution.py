185813cedc0d997204573da845194fbb4bf8d804
Kalyan Chittiboyina <kcy13497@gmail.com>
2023-06-16 13:48:45 -0500

    intial commit

--- execution.py
+++ execution.py
@@ -1,4 +1,3 @@
-#testing git
 import logging 
 logging.basicConfig(filename='execution.log', filemode='a', level=logging.DEBUG,format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
      datefmt='Date%m-%d-%yTime%H:%M:%S')
